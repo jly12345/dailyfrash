22bdf8a082e98439ce9d875f5bf18782201627ab
lingyun.jiang <lingyun.jiang@symbio.com>
2019-11-15 17:53:58 +0800

    TaskId: Desgraption: Author: lingyun.jiang review by:

--- src/main/java/com/work/daily/dailyfrash/entity/DfUser.java
+++ src/main/java/com/work/daily/dailyfrash/entity/DfUser.java
@@ -1,28 +1,170 @@
 package com.work.daily.dailyfrash.entity;
 
-import lombok.Data;
-import lombok.EqualsAndHashCode;
-
-import java.io.Serializable;
+import com.baomidou.mybatisplus.annotation.IdType;
+import com.baomidou.mybatisplus.annotation.TableId;
 import java.time.LocalDateTime;
+import java.io.Serializable;
 
-@Data
-@EqualsAndHashCode(callSuper = false)
+/**
+ * <p>
+ * 
+ * </p>
+ *
+ * @author lingyun.jiang
+ * @since 2019-11-15
+ */
 public class DfUser implements Serializable {
 
-    private static final long serialVersionUID = 358629282525271235L;
+    private static final long serialVersionUID=1L;
 
-    private String userid;
-    private String userName;
-    private String firstName;
-    private String lastName;
-    private String avatar;
-    private Boolean isActive;
-    private LocalDateTime dateJoined;
-    private LocalDateTime createTime;
-    private LocalDateTime updateTime;
-    private Boolean isDelete;
+    @TableId(value = "id", type = IdType.AUTO)
+    private Integer id;
+
     private String password;
+
     private LocalDateTime lastLogin;
+
+    private String userName;
+
+    private String firstName;
+
+    private String lastName;
+
+    private String avatar;
+
     private String email;
+
+    private Boolean isActive;
+
+    private LocalDateTime dateJoined;
+
+    private LocalDateTime createTime;
+
+    private LocalDateTime updateTime;
+
+    private Boolean isDelete;
+
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public LocalDateTime getLastLogin() {
+        return lastLogin;
+    }
+
+    public void setLastLogin(LocalDateTime lastLogin) {
+        this.lastLogin = lastLogin;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getAvatar() {
+        return avatar;
+    }
+
+    public void setAvatar(String avatar) {
+        this.avatar = avatar;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public Boolean getActive() {
+        return isActive;
+    }
+
+    public void setActive(Boolean isActive) {
+        this.isActive = isActive;
+    }
+
+    public LocalDateTime getDateJoined() {
+        return dateJoined;
+    }
+
+    public void setDateJoined(LocalDateTime dateJoined) {
+        this.dateJoined = dateJoined;
+    }
+
+    public LocalDateTime getCreateTime() {
+        return createTime;
+    }
+
+    public void setCreateTime(LocalDateTime createTime) {
+        this.createTime = createTime;
+    }
+
+    public LocalDateTime getUpdateTime() {
+        return updateTime;
+    }
+
+    public void setUpdateTime(LocalDateTime updateTime) {
+        this.updateTime = updateTime;
+    }
+
+    public Boolean getDelete() {
+        return isDelete;
+    }
+
+    public void setDelete(Boolean isDelete) {
+        this.isDelete = isDelete;
+    }
+
+    @Override
+    public String toString() {
+        return "DfUser{" +
+        "id=" + id +
+        ", password=" + password +
+        ", lastLogin=" + lastLogin +
+        ", userName=" + userName +
+        ", firstName=" + firstName +
+        ", lastName=" + lastName +
+        ", avatar=" + avatar +
+        ", email=" + email +
+        ", isActive=" + isActive +
+        ", dateJoined=" + dateJoined +
+        ", createTime=" + createTime +
+        ", updateTime=" + updateTime +
+        ", isDelete=" + isDelete +
+        "}";
+    }
 }
